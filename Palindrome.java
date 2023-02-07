8cba644eef07489a4b2e274cef1e6481bf831b44
Zack O Ogoma <42878810+eliHandy6@users.noreply.github.com>
2023-02-07 12:11:44 +0300

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,17 @@
+public class Palindrome {
+
+    public static void main(String args[]) {
+        System.out.println(isPalindrome("abccba"));
+
+    }
+
+    public static boolean isPalindrome(String str) {
+        if (str.length() == 0 || str.length() == 1) {
+            return true;
+        }
+        if (str.charAt(0) == str.charAt(str.length() - 1)) {
+            return isPalindrome(str.substring(1, str.length() - 1));
+        }
+        return false;
+    }
+}
